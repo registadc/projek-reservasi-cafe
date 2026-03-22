50879b265a33bec96892798d180362c15fc2f647
registadc <cahyanidwiregista08@gmail.com>
2026-03-22 20:41:51 +0700

    masukin projek ke github

---
+++ database/migrations/2026_03_22_133023_create_meja_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meja', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nomor_meja');
+            $table->string('kapasitas', 50);
+            $table->string('status', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meja');
+    }
+};
