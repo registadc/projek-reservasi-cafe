8d3a78dbaabc6060f2e5a2e9ccd1d83203823f69
registadc <cahyanidwiregista08@gmail.com>
2026-03-24 20:04:14 +0700

    menambahkan templat, membuat route, membuat controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UsersController;
 
-Route::get('/', function () {
-    return view('welcome');
+// AUTH
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'processLogin']);
+
+// ADMIN
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
+});
+
+// USER
+Route::middleware(['auth', 'user'])->group(function () {
+    Route::get('/user/dashboard', [UsersController::class, 'dashboard']);
 });
